1eac8aa406cf55ad449e77527bffcb4ee7398ad4
gorgitko <fg-42@seznam.cz>
2021-11-17 15:19:52 +0000

    fixed SCOUT_MARGIN usage

--- binance_trade_bot/auto_trader.py
+++ binance_trade_bot/auto_trader.py
@@ -239,7 +239,7 @@ class AutoTrader(ABC):
 
             if self.config.USE_MARGIN == "yes":
                 ratio_dict[(coin.idx, to_coin.idx)] = (
-                    (1 - transaction_fee) * coin_opt_coin_ratio / ratio - 1 - self.config.SCOUT_MULTIPLIER / 100
+                    (1 - transaction_fee) * coin_opt_coin_ratio / ratio - 1 - self.config.SCOUT_MARGIN / 100
                 )
             else:
                 ratio_dict[(coin.idx, to_coin.idx)] = (
