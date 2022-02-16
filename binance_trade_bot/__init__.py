09f3b4f7ba0d330334a97c56d0684286f7ed5f28
Shrouk Khan <shrouk.khan@agoda.com>
2022-02-16 08:15:31 +0700

    update docker file

--- binance_trade_bot/__init__.py
+++ binance_trade_bot/__init__.py
@@ -2,3 +2,4 @@ from .backtest import backtest
 from .binance_api_manager import BinanceAPIManager
 from .crypto_trading import main as run_trader
 from .database_warmup import warmup_database
+from .backtest_globals import backtest_current_date
