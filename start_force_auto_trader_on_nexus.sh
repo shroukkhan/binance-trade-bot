eed4289e534285c0ffe23c69a8d3a7dc403617df
shroukkhan <shroukkhan@gmail.com>
2022-10-16 11:32:52 +0700

    change docker compose up script

--- start_force_auto_trader_on_nexus.sh
+++ start_force_auto_trader_on_nexus.sh
@@ -1,3 +1,3 @@
-docker-compose -f docker-compose-2.yml up --build --no-cache -d
-docker-compose -f docker-compose.yml up --build --no-cache -d
-docker-compose -f docker-compose-1.yml up --build --no-cache -d
+docker-compose -f docker-compose-2.yml up --build -d
+docker-compose -f docker-compose.yml up --build -d
+docker-compose -f docker-compose-1.yml up --build -d
